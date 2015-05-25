ebb3957e1a920323bd7ec0394822f20aed3001c4
Zachary <zacharyblacktail@gmail.com>
2015-05-25 19:00:47 -0400

    work on .chm. 0.5h

---
+++ TmpHtml/help.h
@@ -0,0 +1,7 @@
+#define introduction1 120
+#define basicgamerulescountries 130
+#define basicgamerulestravelling 140
+#define savingprogress 150
+#define highscores 160
+#define keyshortcuts0 180
+#define contactinfo 190
